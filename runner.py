138e156d636040eb27093029c2763f8730e86b85
lucaslingle <lucasdaxlingle@gmail.com>
2021-11-22 19:06:00 -0800

    commit updates

--- runner.py
+++ runner.py
@@ -1,11 +1,32 @@
+import os
+
 import torch as tc
+
 from utils import get_dataloaders
 from utils import get_mask
-import os
 
 
 class Runner:
-    def __init__(self, dataset_map_fn, batch_map_fn, batch_size, max_tokens, model_name, checkpoint_dir, output_dir):
+    def __init__(
+            self,
+            dataset_map_fn,
+            batch_map_fn,
+            batch_size,
+            max_tokens,
+            model_name,
+            checkpoint_dir,
+            output_dir
+    ):
+        """
+        :param dataset_map_fn: function to apply to dataset rows
+        :param batch_map_fn: function to apply to dataset batches
+        :param batch_size: number of sequences per batch
+        :param max_tokens: number of natural language tokens per sequence,
+                           excluding go tokens and padding.
+        :param model_name: model name for checkpointing.
+        :param checkpoint_dir: checkpoint directory for checkpointing.
+        :param output_dir: output directory for saving samples from the model.
+        """
         self.dataset_map_fn = dataset_map_fn
         self.batch_map_fn = batch_map_fn
         self.batch_size = batch_size
@@ -24,9 +45,9 @@ class Runner:
                 continue
 
             # Forward
-            logprobs, _ = model(X)
+            logits, _ = model(X)
             mask = get_mask(L, sequence_len=X.shape[-1])
-            logprobs_Y = tc.gather(logprobs, dim=-1, index=Y.unsqueeze(dim=-1)).squeeze(dim=-1)
+            logprobs_Y = tc.distributions.Categorical(logits=logits).log_prob(Y)
             masked_nll_loss = -(mask * logprobs_Y).sum()
             loss = masked_nll_loss / mask.sum()
 
@@ -42,25 +63,25 @@ class Runner:
             n = 25000 // self.batch_size
             print(f"batch: [{batch_idx}/{n}]... loss: {loss}")
 
+    @tc.no_grad()
     def _evaluate_epoch(self, model, dataloader, device, fast_eval=True):
         model.eval()
         num_test_tokens = 0
         test_loss, correct = 0, 0
-        with tc.no_grad():
-            for batch_idx, (X, Y, L) in enumerate(dataloader, 1):
-                if fast_eval and batch_idx % 10 != 0:
-                    continue
-                X, Y, L = X.to(device), Y.to(device), L.to(device)
+        for batch_idx, (X, Y, L) in enumerate(dataloader, 1):
+            if fast_eval and batch_idx % 10 != 0:
+                continue
+            X, Y, L = X.to(device), Y.to(device), L.to(device)
 
-                logprobs, _ = model(X)
-                mask = get_mask(L, sequence_len=X.shape[-1])
-                logprobs_Y = tc.gather(logprobs, dim=-1, index=Y.unsqueeze(dim=-1)).squeeze(dim=-1)
-                masked_nll_loss = -(mask * logprobs_Y).sum()
-                loss = masked_nll_loss
+            logits, _ = model(X)
+            mask = get_mask(L, sequence_len=X.shape[-1])
+            logprobs_Y = tc.distributions.Categorical(logits=logits).log_prob(Y)
+            masked_nll_loss = -(mask * logprobs_Y).sum()
+            loss = masked_nll_loss
 
-                test_loss += loss.item()
-                correct += (logprobs.argmax(-1) == Y).type(tc.float).sum().item()
-                num_test_tokens += mask.sum()
+            test_loss += loss.item()
+            correct += tc.eq(logits.argmax(-1), Y).float().sum().item()
+            num_test_tokens += mask.sum()
 
         test_loss /= num_test_tokens
         correct /= num_test_tokens
@@ -74,6 +95,7 @@ class Runner:
         while self.global_step < max_steps:
             print(f"Epoch {epoch}\n-------------------------------")
 
+            # dataloader for this dataset is ephemeral (it's an iterator)
             train_dataloader, test_dataloader = get_dataloaders(
                 dataset_map_fn=self.dataset_map_fn,
                 batch_map_fn=self.batch_map_fn,
@@ -100,30 +122,25 @@ class Runner:
             self.save_checkpoint(model, optimizer)
             epoch += 1
 
+    @tc.no_grad()
     def generate(self, model, vocab, num_samples=10):
-        go_tokens = vocab.stoi['<go>'] * tc.ones((num_samples, 1)).long()
-        i_tokens = vocab.stoi['i'] * tc.ones((num_samples, 1)).long()
-        saw_tokens = vocab.stoi['saw'] * tc.ones((num_samples, 1)).long()
-        this_tokens = vocab.stoi['this'] * tc.ones((num_samples, 1)).long()
-        a_tokens = vocab.stoi['a'] * tc.ones((num_samples, 1)).long()
-        few_tokens = vocab.stoi['few'] * tc.ones((num_samples, 1)).long()
-        tokens = tc.cat((go_tokens, i_tokens, saw_tokens, this_tokens, a_tokens, few_tokens), dim=-1)
+        prefix = ['<go>'] + "i saw this a few".split(" ")
+        prefix = [vocab.stoi(token) for token in prefix]
+        prefix = tc.tile(tc.LongTensor(prefix).view(1, 1), [num_samples, 1])
+        tokens = prefix
+
+        past = None
+        x_tm1 = tokens
 
         model.eval()
-
-        with tc.no_grad():
-            past = None
-            x_tm1 = tokens
-
-            for t in range(tokens.shape[1], self.max_tokens+2):
-                print("t: {}".format(t))
-                # generate tokens x_t, ..., x_{max_tokens}, x_{max_tokens+1}.
-                # in a trained model, ideally the last token would be a '<pad>'
-                logprobs, past = model.forward(x_tm1, past=past)
-                probs = tc.nn.Softmax(dim=-1)(logprobs[:,-1,:])
-                x_t = tc.multinomial(probs, num_samples=1)
-                tokens = tc.cat((tokens, x_t), dim=-1)
-                x_tm1 = x_t
+        for t in range(tokens.shape[1], self.max_tokens+2):
+            print("t: {}".format(t))
+            # generate tokens x_t, ..., x_{max_tokens}, x_{max_tokens+1}.
+            # in a trained model, ideally the last token would be a '<pad>'
+            logits, past = model.forward(x_tm1, past=past)
+            x_t = tc.distributions.Categorical(logits=logits).sample()
+            tokens = tc.cat((tokens, x_t), dim=-1)
+            x_tm1 = x_t
 
         lines = [' '.join([vocab.itos[x] for x in line]) for line in tokens.numpy()]
 
@@ -134,21 +151,26 @@ class Runner:
             for line in lines:
                 f.write('-' * 80 + '\n')
                 f.write(line + '\n')
-
         print('Generated samples were successfully written to {}'.format(fp))
-        return
 
     def save_checkpoint(self, model, optimizer):
-        model_path = os.path.join(self.checkpoint_dir, self.model_name)
-        os.makedirs(model_path, exist_ok=True)
+        model_dir = os.path.join(self.checkpoint_dir, self.model_name)
+        os.makedirs(model_dir, exist_ok=True)
 
-        tc.save(model.state_dict(), os.path.join(self.checkpoint_dir, self.model_name, 'model.pth'))
-        tc.save(optimizer.state_dict(), os.path.join(self.checkpoint_dir, self.model_name, 'optimizer.pth'))
+        model_fp = os.path.join(model_dir, 'model.pth')
+        optimizer_fp = os.path.join(model_dir, 'optimizer.pth')
+
+        tc.save(model.state_dict(), model_fp)
+        tc.save(optimizer.state_dict(), optimizer_fp)
 
     def maybe_load_checkpoint(self, model, optimizer):
         try:
-            model.load_state_dict(tc.load(os.path.join(self.checkpoint_dir, self.model_name, 'model.pth')))
-            optimizer.load_state_dict(tc.load(os.path.join(self.checkpoint_dir, self.model_name, 'optimizer.pth')))
+            model_dir = os.path.join(self.checkpoint_dir, self.model_name)
+            model_fp = os.path.join(model_dir, 'model.pth')
+            optimizer_fp = os.path.join(model_dir, 'optimizer.pth')
+
+            model.load_state_dict(tc.load(model_fp))
+            optimizer.load_state_dict(tc.load(optimizer_fp))
             print('Successfully loaded checkpoint.')
         except Exception:
             print('Bad checkpoint or none. Continuing training from scratch.')
